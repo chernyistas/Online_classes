01460c1744d08379689bb05fdbe6fb82575cc89f
Stas Chernyi <itsmeprivet@mail.ru>
2026-04-03 22:36:27 +0300

    change AbstractBaseUser on AbstractUser

--- users/urls.py
+++ users/urls.py
@@ -1,14 +1,10 @@
 from django.urls import path
-from rest_framework.permissions import AllowAny
-from rest_framework_simplejwt.views import TokenRefreshView, TokenObtainPairView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import (
-    PaymentListAPIView,
-    UserRetrieveAPIView,
-    UserUpdateAPIView,
-    UserCreateAPIView,
-)
+from users.views import (PaymentListAPIView, UserCreateAPIView,
+                         UserRetrieveAPIView, UserUpdateAPIView)
 
 app_name = UsersConfig.name
 
--- users/views.py
+++ users/views.py
@@ -1,15 +1,12 @@
 from django_filters.rest_framework import DjangoFilterBackend
 from rest_framework.filters import OrderingFilter
-from rest_framework.generics import (
-    CreateAPIView,
-    ListAPIView,
-    RetrieveAPIView,
-    UpdateAPIView,
-)
+from rest_framework.generics import (CreateAPIView, ListAPIView,
+                                     RetrieveAPIView, UpdateAPIView)
 from rest_framework.permissions import AllowAny
 
 from users.models import Payment, User
-from users.serializers import PaymentSerializer, UserSerializer, UserRegisterSerializer
+from users.serializers import (PaymentSerializer, UserRegisterSerializer,
+                               UserSerializer)
 
 
 class UserUpdateAPIView(UpdateAPIView):
