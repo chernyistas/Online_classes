73cd2f07db888c8958d248026192cf60f07a25f7
Stas Chernyi <itsmeprivet@mail.ru>
2026-04-08 22:34:47 +0300

    add information into README.md

--- users/urls.py
+++ users/urls.py
@@ -1,13 +1,10 @@
 from django.urls import path
-from rest_framework_simplejwt.views import TokenObtainPairView, TokenRefreshView
+from rest_framework_simplejwt.views import (TokenObtainPairView,
+                                            TokenRefreshView)
 
 from users.apps import UsersConfig
-from users.views import (
-    PaymentListAPIView,
-    UserCreateAPIView,
-    UserRetrieveAPIView,
-    UserUpdateAPIView,
-)
+from users.views import (PaymentListAPIView, UserCreateAPIView,
+                         UserRetrieveAPIView, UserUpdateAPIView)
 
 app_name = UsersConfig.name
 
