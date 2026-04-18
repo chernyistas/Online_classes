746b3d703fe7a8955b1961099d841e58513233fb
Stas Chernyi <itsmeprivet@mail.ru>
2026-04-18 14:12:13 +0300

    Add periodic task to deactivate inactive users

--- config/settings.py
+++ config/settings.py
@@ -140,3 +140,15 @@ EMAIL_USE_TLS = os.getenv("EMAIL_USE_TLS", False) == "True"
 EMAIL_USE_SSL = os.getenv("EMAIL_USE_SSL", False) == "True"
 EMAIL_HOST_USER = os.getenv("EMAIL_HOST_USER")
 EMAIL_HOST_PASSWORD = os.getenv("EMAIL_HOST_PASSWORD")
+
+SERVER_EMAIL = EMAIL_HOST_USER
+DEFAULT_FROM_EMAIL = EMAIL_HOST_USER
+
+CELERY_BEAT_SCHEDULER = "django_celery_beat.schedulers:DatabaseScheduler"
+
+CELERY_BEAT_SCHEDULE = {
+    "deactivate_inactive_users": {
+        "task": "users.tasks.deactivate_inactive_users",
+        "schedule": timedelta(days=1),
+    },
+}
--- users/models.py
+++ users/models.py
@@ -50,6 +50,7 @@ class User(AbstractBaseUser, PermissionsMixin):
     )
     is_active = models.BooleanField(default=True)
     is_staff = models.BooleanField(default=False)
+    last_login = models.DateTimeField(auto_now=True, verbose_name="Последнее посещение")
 
     USERNAME_FIELD = "email"
     REQUIRED_FIELDS: list[str] = []
